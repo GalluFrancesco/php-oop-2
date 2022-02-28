0ff945618d3b95ce0ac2b1b8b72c9ce9d3a819d9
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-28 17:40:33 +0100

    Add: items in User's cart

--- index.php
+++ index.php
@@ -10,7 +10,11 @@ $item3= new Food("cibo3", 7.35, 1003, 15);
 $item4= new Toy("gioco1", 12.40, 2001, "gomma");
 $item5= new Toy("gioco2", 12.40, 2002, "legno");
 
-$user= new User("Pippo Baudo", true);
+$user = new User("Pippo Baudo", true);
+
+$user=>addItem($item1);
+$user=>addItem($item3);
+$user=>addItem($item4);
 
 var_dump($user);
 
