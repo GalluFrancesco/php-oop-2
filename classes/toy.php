1c284e552a6d9f61b45437e625df80ae8771bca4
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-28 16:37:28 +0100

    Add: class Toy

---
+++ classes/toy.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . "/classes/product.php";
+
+class Food extends Product{
+
+    private string $material;
+    private string $typeCode;
+
+    function __construct($name, $price, $barCode, $material) {
+        parent::setName($name);
+        parent::setPrice($price);
+        parent::setName($barCode);
+        $this->weight=$this->setWeight($material);;
+        $this->typeCode="gioco";
+      }
+
+      public function getMaterial(){
+        return $this->material;
+      }
+    
+      public function setMaterial($material){
+        $this->material = $material;
+      }
+}
