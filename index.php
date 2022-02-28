6a17a2fcbe9d1189a9474c5a3dd07bc581621b2b
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-28 17:12:54 +0100

    Add: items in index

--- index.php
+++ index.php
@@ -4,5 +4,8 @@ require_once __DIR__ . "/classes/food.php";
 require_once __DIR__ . "/classes/toy.php";
 
 $item1= new Food("cibo1", 12.40, 1001, 5);
+$item2= new Food("cibo2", 10.00, 1002, 10);
+$item3= new Food("cibo3", 7.35, 1003, 15);
+$item4= new Toy("gioco1", 12.40, 2001, "gomma");
+$item5= new Toy("gioco2", 12.40, 2002, "legno");
 
-var_dump($item1);
