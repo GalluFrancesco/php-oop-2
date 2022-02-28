19eca9166f41a88a83d4736ccde06c2988be55c5
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-28 16:50:40 +0100

    Fix: class name & require

--- classes/food.php
+++ classes/food.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/classes/product.php";
+require_once __DIR__ . "/product.php";
 
 class Food extends Product{
 
--- classes/toy.php
+++ classes/toy.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . "/classes/product.php";
+require_once __DIR__ . "/product.php";
 
-class Food extends Product{
+class Toy extends Product{
 
     private string $material;
     private string $typeCode;
